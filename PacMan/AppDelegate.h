13a8ccbd7f82945b7c78407b982d376af983a8e2
Reinual <michel.launier@videotron.ca>
2011-05-11 08:03:23 -0400

    Initial Commit

---
+++ PacMan/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  PacMan
+//
+//  Created by Michel Launier on 11-05-11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ PacMan/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  PacMan
+//
+//  Created by Michel Launier on 11-05-11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
